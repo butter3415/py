1db1c8d8e9a2abb497433ef9b69590444df1857c
butter3415 <butter3415@gmail.com>
2023-01-19 20:37:50 +0900

    day04 list03

--- day04.py
+++ day04.py
@@ -1,51 +1,40 @@
-import random
+import copy
 
-# hard coding
-# numbers = list()
-# numbers.append(random.randint(1, 100)) # 100 포함 랜덤수
-# numbers.append(random.randint(1, 100))
-# numbers.append(random.randint(1, 100))
-# numbers.append(random.randint(1, 100))
-# numbers.append(random.randint(1, 100))
-# print(numbers)
-
-# for loop
-# numbers = []
-# for k in range(5):
-#     numbers.append(random.randint(1,100))
-# print(numbers)
-
-# list comprehension
-# numbers = [random.randint(1,100) for k in range(5)]
-# print(numbers)
-
-# odds = [k for k in range(1,11) if k % 2]
-# for k in range(1, 11):
-#     if k % 2 :
-#         odds.append(k)
-# print(odds)
-
-odds = [k for k in range(1,11) if k % 2] # 변수 - for문 - if문 /// k % 2 == 1 -> True
+odds = list(range(1, 11, 2))
 print(odds)
 
-# join 은 문자열에서 사용 가능하기 때문에 정수타입 리스트를 문자열로 변경해서 출력
+# mixed = [3, 4, 1, 7, 'A', '한', '김', 5, 'b', 'B']    # 수치형은 int/ 문자는 str => 비교가 안됨
+# TypeError: '<' not supported between instances of 'str' and 'int'
 
-# 해결방법 1. index
-# for i in range(len(odds)):
-#     odds[i] = str(odds[i])
-# print(odds)
-# print(' / '.join(odds)) # 리스트의 원소들을 하나의 문자열로 만들어준다.
+mixed = ['3', '4', '1', '7', 'A', '한', '김', '5', 'b', 'B']
+# ascending : arabic number, alphabet UpperCase, alphabet LowerCase, Korean
+# 오름차순 : 숫자, 대문자, 소문자, 한글
+mixed.sort(reverse=True) # descending
+# print(mixed)
 
-# 해결방법 2. list
-# temp = list()
-# for odd in odds:
-#     temp.append(str(odd))
-# print(temp)
-# print(' / '.join(temp))
+# copy 방법
+# a = [1,2,3]
+# b = a.copy() # copy 함수 써서 복제
+# c = list(a)  # 리스트 이용해서 복제본 만들었음
+# d = a[:]    # 1 에서 끝까지 복제
+#
+# a[1] = 'inha'
+# c[2] = 'univ'
+#
+# print(a, b, c, d)
 
-# 해결방법 3. enumerate
-for item in enumerate(odds):    # (index, value) 튜플 형태로 리턴
-    # print(item) # 튜플 형태로 출력 (0,1) (1,3) (2,5) ...
-    odds[item[0]] = str(item[1])
-print(odds)
-print(' / '.join(odds))
+# a = [1, [9, -7], 3]
+# b = a.copy() # copy 함수 써서 복제
+# c = list(a)  # 리스트 이용해서 복제본 만들었음
+# d = a[:]    # 1 에서 끝까지 복제
+#
+# # a[1] = 'inha'
+# a[1][0] = -99   # 다른 복제본에도 영향을 줌. b, c, d [1][0]번 방에도 -99로 변경
+#
+# print(a, b, c, d)
+
+a = [1, [9, -7], 3]
+b = copy.deepcopy(a)
+
+a[1][1] = 'inha'
+print(a, b)
