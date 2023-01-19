6281512d80c8ca1d2051afbbe59a73c16f62d24a
butter3415 <butter3415@gmail.com>
2023-01-19 21:06:41 +0900

    day04 dictionary01

--- day04.py
+++ day04.py
@@ -1,40 +1,21 @@
-import copy
+groups = {'빅뱅':['GD','태양','탑','대성','승리'],
+          '마마무':['문별','솔라','휘인','화사']}
 
-odds = list(range(1, 11, 2))
-print(odds)
+# print(groups['빅뱅'].pop(-1))
+# print(groups)
+# print(groups.pop('마마무'))
+# print(groups)
+# print(groups.pop('마마무')) # KeyError: '마마무'
+# print(groups)
 
-# mixed = [3, 4, 1, 7, 'A', '한', '김', 5, 'b', 'B']    # 수치형은 int/ 문자는 str => 비교가 안됨
-# TypeError: '<' not supported between instances of 'str' and 'int'
+# for group, members in groups.items():
+#     print(f'{group}의 멤버')
+#     for member in members :
+#         if member != '승리':
+#             print(member)
 
-mixed = ['3', '4', '1', '7', 'A', '한', '김', '5', 'b', 'B']
-# ascending : arabic number, alphabet UpperCase, alphabet LowerCase, Korean
-# 오름차순 : 숫자, 대문자, 소문자, 한글
-mixed.sort(reverse=True) # descending
-# print(mixed)
-
-# copy 방법
-# a = [1,2,3]
-# b = a.copy() # copy 함수 써서 복제
-# c = list(a)  # 리스트 이용해서 복제본 만들었음
-# d = a[:]    # 1 에서 끝까지 복제
-#
-# a[1] = 'inha'
-# c[2] = 'univ'
-#
-# print(a, b, c, d)
-
-# a = [1, [9, -7], 3]
-# b = a.copy() # copy 함수 써서 복제
-# c = list(a)  # 리스트 이용해서 복제본 만들었음
-# d = a[:]    # 1 에서 끝까지 복제
-#
-# # a[1] = 'inha'
-# a[1][0] = -99   # 다른 복제본에도 영향을 줌. b, c, d [1][0]번 방에도 -99로 변경
-#
-# print(a, b, c, d)
-
-a = [1, [9, -7], 3]
-b = copy.deepcopy(a)
-
-a[1][1] = 'inha'
-print(a, b)
+for temps in groups.items():
+    print(f'{temps[0]}의 멤버')
+    for member in temps[1] :
+        if member != '승리':
+            print(member)
