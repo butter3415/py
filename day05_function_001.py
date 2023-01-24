d5881fd9174e0510a4e0524668d8ffeda67adccf
butter3415 <butter3415@gmail.com>
2023-01-24 18:51:47 +0900

    day05 function02(map, filter 함수)

--- day05_function_001.py
+++ day05_function_001.py
@@ -1,58 +1,33 @@
-# 함수 (Function cont.)
-# 재귀함수 (자기 자신을 호출하는 함수)
+# # 함수의 매개변수로 함수 전달하기
+# def print_hi(hi):
+#     for k in range(5):
+#         hi()
+#
+# def hi():
+#     print('hello~')
+#
+# # hi()
+# print_hi(hi)
 
-# factorial(팩토리얼)
-# 5! = 5 X 4 X 3 X 2 X 1 = 120
+# map(함수, 순환가능한 자료구조)
+# 리스트, 딕셔너리, 문자열, 범위 range
+def square(n):
+    return n * n
+def odd(n):
+    return n % 2 == 1
 
-def factorial_recursion(n):
-    """
-    팩토리얼 by 재귀
-    f(n) = n * f(n-1)
-    f(3) = 3 * f(2)
-         = 3 * 2 * f(1)
-         = 3 * 2 * 1 * f(0)
-         = 3 * 2 * 1 * 1
-    """
-    if n == 0:
-        return 1
-    else:
-        return n * factorial_recursion(n-1)
-def factorial_loop(n):
-    """
-    팩토리얼 by 반복문
-    """
-    result = 1
-    for i in range(1, n+1):
-        result = result * i
-    return result
+# result = []
+# for k in range(1, 6):
+#     result.append(square(k))
+# print(result)
 
-print(factorial_recursion(4))
-print(factorial_recursion(6))
-print(factorial_loop(4))
-print(factorial_loop(6))
+print(list(map(square, [1,2,3,4,5])))
+# map 함수는 list로 변경해야 사용 가능
+# map 함수는 코드가 간결해짐
 
-# fibonacci(피보나치)
+# 17-20 라인 4줄과 22라인 1줄의 실행 결과는 동일
 
-def fibo_recursion(n):
-    """
-    f(n) = f(n-1) + f(n-2)
-    f(1) = 1
-    f(2) = 1
-    f(3) = f(2) + f(1)
-         = 1 + 1
-    f(4) = f(3) * f(2)
-    """
-    if n == 1:
-        return 1
-    if n == 2:
-        return 1
-    else:
-        return fibo_recursion(n-1) + fibo_recursion(n-2)
+# filter(함수, 순환가능한 자료구조)
+print(list(filter(odd, [1,2,3,4,5])))
 
-for k in range(1,8):
-    print('피보나치 {0} : {1}'.format(k, fibo_recursion(k)))
-
-# 재귀함수는 성능 상의 문제가 있음 (숫자가 클수록 오버헤드가 많이 걸림)
-#   -> 해결 방법 중 하나가 메모화
-
-
+# 람다함수, 제너레이터
