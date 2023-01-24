2d6b2dcbd05b95b1d426fd640c00579344932aa7
butter3415 <butter3415@gmail.com>
2023-01-24 21:19:06 +0900

    day06 fileIO(음식추천프로그램v0.5)

--- day06_fileIO.py
+++ day06_fileIO.py
@@ -1,32 +1,31 @@
-# file io
-# Open, Close 파일 있음.
+# file io(cont.)
+# 음식 추천 프로그램 만들기 v0.5 ~
 
-# 파일 객체명 = open(파일 경로, 읽기 모드) * 파일 경로, 읽기 모드 - 문자열
-# w : 쓰기 모드(write), r : 읽기모드(read), a : 이어쓰기 모드(append)
-# 파일을 닫을 때 : 파일객체명.close()
+import random
 
-# 파일 쓰기
-# fp = open('war_flower.txt', 'w')
-# print('고니', file=fp) # 실제 쓰기
-# print('정마담', file=fp) # 실제 쓰기
-# print('아귀', file=fp) # 실제 쓰기
-# fp.write('너부리') # 실제 쓰기
-# fp.close() # 열었으니까 닫기
+alcohol_foods = {}
 
-# 파일 읽기 (default 모드가 r (읽기 모드))
-# fp = open('war_flower.txt', 'r')
-# lines = fp.readlines() # 파일을 1행 단위의 리스트 원소로 리턴
-# # print(lines)
-#
-# for line in lines:
-#     # print(line.rstrip('\n'))
-#     # print(line[:-1])
-#     # print(line, end='')
-#
-# fp.close()
+with open('alcohols.txt', 'r') as fp1:
+    with open('foods.txt', 'r') as fp2:
+        alcohols = fp1.readlines()  # 리스트 변수 alcohols
+        foods = fp2.readlines() # 리스트 변수 foods
+        for k in range(len(alcohols)):
+            alcohol_foods[alcohols[k].strip('\n')] = foods[k][0:-1]   # strip('\n') -> \n 떨궈내기 위함
+# print(alcohols)
+# print(foods)
+# print(alcohol_foods)
 
-# with
-with open('war_flower.txt') as fp:
-    lines = fp.readlines()
-    for line in lines:
-        print(line[:-1])
+
+
+while True:
+    alcohol = input('주문할 술(맥주/와인/고량주/소주/랜덤/결제)은? ')
+    if alcohol == '결제':
+        print("안녕히 가십시오~")
+        break
+    if alcohol in alcohol_foods.keys():    # alcohol_food 키 중에 있으면 실행
+        print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+    elif alcohol == '아무거나':
+        any = random.choice(list(alcohol_foods))
+        print('{0}을(를) 추천합니다. 안주는 {1}입니다.'.format(any, alcohol_foods[any]))
+    else :
+        print('{0}은(는) 판매하지 않습니다. 메뉴에서 골라주세요~'.format(alcohol))
