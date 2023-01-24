6310bac9907312eb2c0fa3f15c0888eaad0b993b
butter3415 <butter3415@gmail.com>
2023-01-24 19:01:16 +0900

    dqy05 module preview

---
+++ day05_modulePreview.py
@@ -0,0 +1,12 @@
+# module preview
+# 파이썬 코드 파일 - 모듈
+# 모듈 이용하여 파일 분리, 공유
+
+# built in module 내장(표준)모듈
+import math
+import random
+
+print(math.ceil(3.1))   # 올림 함수(ceil)
+print(math.floor(4.9))
+print(math.sqrt(16))     # square root - sqrt 제곱근
+print(random.randint(1, 6))
