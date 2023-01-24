5ad4a6181ee442d68d84fc0bc5bb0ccdba761c9c
butter3415 <butter3415@gmail.com>
2023-01-24 22:30:00 +0900

    day06 Exception Handling02

--- day06_exceptionHandling.py
+++ day06_exceptionHandling.py
@@ -1,42 +1,30 @@
-# exception handling
+# exception handling (cont.)
 
-# try:
-# 예외가 발생할 가능성이 있는 코드
-# except:
-# 예외가 발생했을 때 실행할 코드
-# else:
-# 예외가 발생하지 않았을때 실행할 코드
-# finaly:
-# 무조건 실행 / 예외가 발생 여부에 상관없이 실행하는 코든
+def midterm():
+    score = int(input('1에서 10 사이의 정수 입력 : '))
+    if score < 0 or score > 10 :
+        raise Exception("중간고사 점수 입력 범위를 벗어났습니다. {0}점 입력 불가".format(score))
+    else:
+        print('{0}점 입니다.'.format(score))
 
 try:
-    c = list()
-    c.append("사과")
-    a = int(input())
-    b = int(input())
-    print(a / b)
-    print(c[0])
-except ZeroDivisionError:
-    print("분모에 0이 올 수 없습니다.")
-except ValueError:
-    print("입력된 수는 정수가 아닙니다!")
-except IndexError:
-    print("리스트의 범위를 벗어난 인덱스가 사용되었습니다.")
-except:
-    print("무언가 에러가 발생했습니다.")
-else:
-    print("정상적으로 처리되었습니다.")
-finally:
-    print("예외 발생 여부에 상관없이 항상 실행됩니다.")
+    midterm()
+except Exception as e:
+    print("예외 발생 : {0}".format(e))
+
+# try:
+#     print(5/2)
+#     b = int(input())
+#     a = [1, 2, 3]
+#     print(a[1])
+#     raise Exception("지혜가 만든 예외")    # raise 일으키다 -> 예외를 일으키다
+# except IndexError as e:
+#     print("인덱스 범위를 벗어났습니다 : {0}".format(e))
+# except ZeroDivisionError as e:
+#     print("분모에 0이 올 수 없습니다 : {0}".format(e))
+# except Exception as e:  # 맨 마지막에 작성 / 전체 조상 (다 잡아줌)
+#     print("무언가 에러가 발생했습니다. : {0}".format(e))
 
 
-# a = input()
-# b = input()
 
-# print(a.isdigit(), b.isdigit())
-#
-# if a.isdigit() and b.isdigit(): # isdigit 는 숫자인지만 보기 때문에 -99 를 입력하면 False 가 나옴
-#     print(int(a) + int(b))
-# else:
-#     print("입력된 수는 정수가 아닙니다.")
 
