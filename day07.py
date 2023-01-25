d1a0cf02e9be355ad0cb34a8f414f5d0c99b82fc
butter3415 <butter3415@gmail.com>
2023-01-25 20:22:00 +0900

    day07 function-놀이공원 인원수, 요금 계산 프로그램

--- day07.py
+++ day07.py
@@ -1,12 +1,25 @@
-def calculate_fee(*ages):   # tuple로 받는다
+import random
+
+def calculate_fee(ages):   # list로 받는다
     total = 0
+    adults = 0
+    kids = 0
+
     for age in ages:
         if  19 <= age :    # adult
             total = total + 10000
+            adults += 1
         else:   # kids
             total = total + 4000
-    return total
+            kids += 1
+    return [len(ages), adults, kids, total]
 
+# result = calculate_fee([7, 45, 43, 10])
+no_of_visitor = int(input(f'몇 분이신가요~?! '))
+ages = [random.randint(1, 100) for age in range(no_of_visitor)]
+results = calculate_fee(ages)
+print(ages)
+print(f'{results[0]}명 방문 하셨고, 어른 {results[1]}명, 어린이 {results[2]}명, 총 요금은 {results[-1]}원입니다.')
 
-print(calculate_fee(21, 25, 20))
-print(calculate_fee(7, 45, 43, 10))
+# 출력 양식
+# 3명 방문하셨고 어른 3명, 어린이 0명 총 요금은 30000원입니다.
