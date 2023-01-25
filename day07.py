0780024ac60f3a1bd9ec81cda8029b4595111354
butter3415 <butter3415@gmail.com>
2023-01-25 19:50:18 +0900

    day07 function01-v1.3(with 빛태간교수님ㅜㅠ)

--- day07.py
+++ day07.py
@@ -1,25 +1,52 @@
-# prime number v1.0
+# prime number v1.3
+# gui (button 1, entry 2, label 1)
+import tkinter as tk
+
 
 def isprime(n):
     """
     매개변수로 받은 정수가 소수인지 판정하는 함수
     :param n: integer number
-    :return:
+    :return: True or False
     """
     if n <= 1:
         return False
+
     for k in range(2, n):
         if n % k == 0:
             return False
+
     return True
 
-number_list = input('input 2 integer number : ').split()
-start = int(number_list[0])
-end = int(number_list[1])
 
-if end < start:
-    start, end = end, start  # tuple packing & unpacking
+def click_prime():
+    start = int(en_start.get())
+    end = int(en_end.get())
 
-for number in range(start, end+1):
-    if isprime(number):     # 함수 호출
-        print(number, end = ' ')
+    if end < start:
+        start, end = end, start
+
+    results = ''
+    for number in range(start, end + 1):
+        if isprime(number):
+            #results = results + str(number) + ' '
+            results = results + f'{number} '
+
+    lbl_result.config(text=results)
+
+
+win = tk.Tk()
+win.title('구간 소수 계산 프로그램')
+win.geometry('300x150')
+
+lbl_result = tk.Label(win, text='아래 두 정수를 입력하세요')
+en_start = tk.Entry(win)
+en_end = tk.Entry(win)
+btn_prime = tk.Button(win, text='소수 계산', command=click_prime)
+
+lbl_result.pack()
+en_start.pack()
+en_end.pack()
+btn_prime.pack(fill='x')
+
+win.mainloop()
