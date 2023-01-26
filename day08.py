20cefd0508ea1b01604bfee36c282c46bc312f95
butter3415 <butter3415@gmail.com>
2023-01-26 21:55:32 +0900

    day08 class04(Pokemon)

--- day08.py
+++ day08.py
@@ -1,3 +1,4 @@
+# class & object
 class Pokemon:
     def __init__(self, owner, skills):  # constructor
         self.hidden_owner = owner  # attribute, field, member variable
@@ -5,10 +6,12 @@ class Pokemon:
         import datetime  # 상속관계 x
         print(f'[{datetime.datetime.now()}] 포켓몬 생성됨 :', end=' ')
 
-    def get_owner(self):
+    @property
+    def owner(self):
         return self.hidden_owner
 
-    def set_owner(self, owner):
+    @owner.setter
+    def owner(self, owner):
         self.hidden_owner = owner
 
     def info(self):  # method (member function)
@@ -20,29 +23,29 @@ class Pokemon:
         for skill in self.skills:
             print(f'===========\n{skill}')
         print('===========')
+
     def attack(self, idx):
         print(f'{self.skills[idx]} 공격 시전!')
 
-    owner = property(get_owner, set_owner)
 
-class Pikachu(Pokemon):  # class 자식클래스(부모클래스):
+class Pikachu(Pokemon):     # class 자식클래스(부모클래스):
     def __init__(self, owner, skills):
         super().__init__(owner, skills)
         self.name = "피카츄"
         print(f'{self.name}')
 
-    def attack(self, idx):  # override
+    def attack(self, idx):      # override
         print(f'[삐까삐까] {self.hidden_owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
 
-
-class Ggoboogi(Pokemon):  # inheritance
+class Ggoboogi(Pokemon):        # inheritance
     def __init__(self, owner, skills):
         super().__init__(owner, skills)
         self.name = "꼬부기"
         print(f'{self.name}')
 
-    def attack(self, idx):  # override
+    def attack(self, idx):      # override
         print(f'[꼬북꼬북] {self.hidden_owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+
 
 pika1 = Pikachu('한지우', '번개/100만 볼트')
 ggo1 = Ggoboogi('오바람', '거품/물대포/몸통박치기')
@@ -50,13 +53,11 @@ ggo1 = Ggoboogi('오바람', '거품/물대포/몸통박치기')
 # pika1.info()
 pika1.attack(1)
 ggo1.attack(2)
-
 # #print(pika1.hidden_owner)  # public, 직접적인 접근 가능
 # print(pika1.get_owner())  # getter 접근 가능
 # pika1.set_owner('덴트')  # setter로 변경
 # # pika1.hidden_owner = '덴트'
 # print(pika1.get_owner())  # getter 접근 가능
-
 print(pika1.owner)  # property 이용
 pika1.owner = '덴트'  # property 이용
 print(pika1.owner)  # property 이용
