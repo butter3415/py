c7b30ecd5d5575edb7828978d20f341e6016dc6e
butter3415 <butter3415@gmail.com>
2023-01-31 20:33:23 +0900

    day10 web03

--- day10.py
+++ day10.py
@@ -1,13 +1,27 @@
-import urllib.request
-import urllib.parse
+from bs4 import BeautifulSoup
 
-api = 'https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp'
+html = """
+<html>
+<head>
+<title>스크레이핑 실습</title>
+</head>
+<body>
+<h1>인하대학교</h1>
+<h2>소프트웨어융합공학과</h2>
+<p>웹 스크레이핑</p>
+<p>파이썬 기본 문법, 판다스, 스크레이핑, GUI ... </p>
+</body>
+</html>
+"""
 
-id = input('지역코드 : ')   # 108 전국, 109 서울인천경기, 105 강원도
-values = {'stnId' : id}
-parameters = urllib.parse.urlencode(values) # 딕셔너리[문자열, 문자열]
-url = api + '?' + parameters
+soup = BeautifulSoup(html, 'html.parser')
+t = soup.html.head.title
+h1 = soup.html.body.h1.string
+p1 = soup.html.body.p   # p 태그 두개면 위에 있는 p 태그 내용 나옴
+p2 = p1.next_sibling.next_sibling
 
-urls = urllib.request.urlopen(url).read()
-texts = urls.decode('utf-8')
-print(texts)
+print(p1.string)
+print(p2.string)
+print(h1)
+print(t)
+print(t.string)
