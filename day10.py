72115c0c6aef713bbd78b51f7c0262238c48460f
butter3415 <butter3415@gmail.com>
2023-01-31 21:05:52 +0900

    day10 web04

--- day10.py
+++ day10.py
@@ -6,22 +6,25 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
-<h1>인하대학교</h1>
-<h2>소프트웨어융합공학과</h2>
-<p>웹 스크레이핑</p>
-<p>파이썬 기본 문법, 판다스, 스크레이핑, GUI ... </p>
+<a href="http://www.inha.ac.kr">인하대학교</a><br>
+<a href="http://www.harvard.edu">하버드대학교</a>
 </body>
 </html>
 """
 
-soup = BeautifulSoup(html, 'html.parser')
-t = soup.html.head.title
-h1 = soup.html.body.h1.string
-p1 = soup.html.body.p   # p 태그 두개면 위에 있는 p 태그 내용 나옴
-p2 = p1.next_sibling.next_sibling
+# 인하대학교의 url 주소는 http://www.inha.ac.kr입니다.
 
-print(p1.string)
-print(p2.string)
-print(h1)
-print(t)
-print(t.string)
+soup = BeautifulSoup(html, 'html.parser')
+urls = soup.find_all("a")  # return list 리스트
+
+# print(urls)
+for url in urls:
+    print('{0}의 url주소는 {1}입니다.'.format(url.string, url.attrs['href']))
+
+
+# university = soup.find(id='univ')
+# contents = soup.find(id='contents')
+#
+# print(university.string)
+# print(contents.string)
+
