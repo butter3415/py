71d35a5d706509ef38e4e8ae2610d2d1dab40c7b
butter3415 <butter3415@gmail.com>
2023-01-31 19:05:09 +0900

    day09 pandas

--- day09.py
+++ day09.py
@@ -1,117 +1,45 @@
-# Pokemon game v0.3
+# pandas
+# pip install pandas #
+import pandas as pd
 
-class Pokemon:
-    count = 0   # class variable total count of pokemon # 객체 공유
+# pd.DataFrame # 행과 열의 데이터 프레임 만들어짐
+'''
+df = pd.DataFrame(
+    {'a' = [4, 5, 6],
+     'b' = [7, 8, 9],
+     'c' = [10, 11, 12]},
+     index = [1, 2, 3]) # 인덱스 안주면 0번으로 시작
 
-    def __init__(self, owner, skills):  # constructor
-        self.__owner = owner  # like private
-        self.skills = skills.split('/')  # list
-        import datetime  # 상속관계 x
-        print(f'[{datetime.datetime.now()}] 포켓몬 생성됨 :', end=' ')
-        Pokemon.count = Pokemon.count + 1
+    a   b   c
+1   4   7   10
+2   5   8   11
+3   6   9   12
+'''
 
-    @classmethod    # 클래스 객체로 공유한다.
-    def no_pokemons(cls):
-        print(f'현재 총 {cls.count}마리의 포켓몬이 생성 되었습니다.')
+# df = pd.DataFrame({"a" : [4, 5, 6],
+#                    "b" : [7, 8, 9],
+#                    "c" : [10, 11, 12]},
+#                    index = [1, 2, 3]) # 딕셔너리 스타일
 
-    @property
-    def owner(self):
-        return self.__owner
+df = pd.DataFrame([[4, 5, 6],
+                   [7, 8, 9],
+                   [10, 11, 12]],
+                   index = [1, 2, 3],
+                   columns=['a','b','c'])   # 2차원 리스트 형태
+print(df)
+# print(df.loc[: , ['a', 'c']])
+print(df.loc[df['b']>8, ['a', 'c']])    # b의 수가 8 이상인 a,c열의 수 가져오기 
 
-    @owner.setter
-    def owner(self, owner):
-        self.__owner = owner
+df1 = (pd.melt(df)
+       .rename(columns={
+               'variable' : 'var',
+               'value' : 'val' })
+       .iloc[:,[1]]
+       # .iloc[1:5]
+       #.query('val >= 7')
+)
+# pivot 은 melt 의 반대
+print(df1)
 
-    def info(self):  # method (member function)
-        """
-        포켓몬 정보(주인, 이름, 기술들) 출력
-        :return: void
-        """
-        print(f'{self.__owner}의 포켓몬이 사용 가능한 스킬')
-        for i in range(len(self.skills)):
-            print(f'{i+1} : {self.skills[i]}')
-
-    def attack(self, idx):
-        print(f'{self.skills[idx]} 공격 시전!')
-
-
-class Pikachu(Pokemon):  # class 자식클래스(부모클래스):
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "피카츄"
-        print(f'{self.name}')
-
-    def attack(self, idx):  # override
-        print(f'[삐까삐까] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
-
-    # def equals(self,pk):
-    #     return self.owner == pk.owner
-
-    def __eq__(self, other):
-        return self.owner == other.owner
-
-    def __gt__(self, other):
-        return len(self.skills) > len(other.skills)
-
-    def __repr__(self):
-        return f'이 피카츄는 {len(self.skills)}개의 스킬을 가지고 있는 {self.owner}의 피카츄입니다.'  # toString 이랑 비슷
-
-
-class Ggoboogi(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "꼬부기"
-        print(f'{self.name}')
-
-    def attack(self, idx):  # override
-        print(f'[꼬북꼬북] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
-
-
-class Pairi(Pokemon):  # inheritance
-    def __init__(self, owner, skills):
-        super().__init__(owner, skills)
-        self.name = "파이리"
-        print(f'{self.name}')
-
-    def attack(self, idx):  # override
-        print(f'[파읠파읠] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
-
-
-
-if __name__ == "__main__":
-    pa = Pikachu('한지우', '번개/50만볼트')
-    pb = Pikachu('아이리스', '번개/50만볼트/100만볼트')
-
-    print(pa)
-    # pc = pa == pb   # 양쪽 피카츄 객체의 주인이 같으면 True
-    # pc = pa.equals(pb)
-    # pc = pa < pb    # 의미 헤치지 말기
-
-    # pc = 1 + 2
-    # pc = '9' + '7'
-
-    # print(pc)
-
-    while True:
-        Pokemon.no_pokemons()
-        menu = input('1) 포켓몬 생성  2) 프로그램 종료 : ')
-        if '2' == menu:
-            print('프로그램을 종료합니다')
-            break
-        elif '1' == menu:
-            pokemon = input('1) 피카츄  2) 꼬부기  3) 파이리 : ')
-            n = input('플레이어 이름 입력 : ')
-            ss = input('사용 가능한 기술 입력(/로 구분하여 입력) : ')
-            if pokemon == '1':
-                p = Pikachu(n, ss)
-            elif pokemon == '2':
-                p = Ggoboogi(n, ss)
-            elif pokemon == '3':
-                p = Pairi(n, ss)
-            else:
-                continue
-            p.info()  # skill selection
-            attack_no = int(input('공격 번호 선택 : '))
-            p.attack(attack_no-1)
-        else:
-            print('메뉴에서 골라 주세요')
+print(df1.sort_values('val'))   # 오름차순 정렬
+print(df1.sort_values('val',ascending=False))   # 내림차순 정렬
